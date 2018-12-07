c8feec4e4bf7b11f1d6acc70966c00f4d5e37841
ralphwengerek <ralph.wengerek@gmail.com>
2018-12-07 11:37:53 +0000

    fix reference

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import mapChooser from "../../Lib/mapChooser";
+import mapChooser from "../../lib/mapChooser";
 import "./Map.scss";
 
 const defaultImage = "/images/default.png";
